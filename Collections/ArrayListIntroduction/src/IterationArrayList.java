b710eef932c3ff92d68a23b28a2aa87b8e0b165d
Kaan Çoban <kaaan3452@gmail.com>
2025-07-25 09:22:16 +0300

    Add files via upload

---
+++ Collections/ArrayListIntroduction/src/IterationArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IterationArrayList {
+    public static void main(String[] args) {
+        List <Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        for(Integer i : list){
+            System.out.println(i);
+        }
+    }
+}
