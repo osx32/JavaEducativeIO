1139cb78f52d9c64af7ec6b4c360f05dfefca0f6
Kaan Çoban <kaaan3452@gmail.com>
2025-07-31 09:39:37 +0300

    30/07/2025 code

---
+++ Multithreading/CyclicBarrier/src/Task.java
@@ -0,0 +1,30 @@
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Task implements Runnable{
+
+    private CyclicBarrier barrier;
+
+    public Task(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    //Await is invoked to wait for other threads
+    @Override
+    public void run(){
+        try {
+            System.out.println(Thread.currentThread().getName() + " is waiting on barrier");
+            barrier.await();
+            //printing after crossing the barrier
+            System.out.println(Thread.currentThread().getName() + " has crossed the barrier");
+        }
+        catch (InterruptedException ex) {
+            Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (BrokenBarrierException ex) {
+            Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
